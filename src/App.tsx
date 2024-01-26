6ae5bcbd89486bb8dc8dbd7d20b972b6708ad0a1
Judith Böhlert <hello@judithboehlert.com>
2024-01-26 12:23:18 +0100

    Add custom colours and bg gradient.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline ">Hallo Test</h1>
-    </>
+    <div className="dark">
+      <div className="h-screen w-screen  bg-gradient-to-br from-20% dark:from-neutral-900 dark:via-blue-900 dark:to-teal-900 ">
+        <h1 className="text-3xl font-bold underline dark:text-white ">
+          Hallo Test
+        </h1>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      neutral: {
+        100: "#EAF1FB",
+        300: "#E0E6F1",
+        500: "#94A2B9",
+        700: "#4D5D76",
+        900: "#041B3F"
+      },
+      blue: {
+        100: "#C5D9F8",
+        500: "#44669C",
+        900: "#143569"
+      },
+      teal: {
+        100: "#CDE9F3",
+        500: "#3586A5",
+        900: "#054963"
+      },
+      rose: {
+        100: "#FFF1FD",
+        500: "#DDA9D5",
+        900: "#B066A5"
+      }
+    },
     extend: {}
   },
   plugins: []
