992d5af32bf731f90e5cc4f877cece5bd27aec1e
Judith Böhlert <hello@judithboehlert.com>
2024-02-01 16:43:08 +0100

    Update page layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import judiImage from "./assets/judi_couch_square_blurred.webp";
+
 function App() {
   return (
-    <div className="dark">
-      <div className="h-screen w-screen  bg-gradient-to-br from-20% dark:from-neutral-900 dark:via-blue-900 dark:to-teal-900 ">
-        <h1 className="text-3xl font-bold underline dark:text-white ">
-          Hallo Test
-        </h1>
+    <div className="dark ">
+      <div className="min-w-screen min-h-screen  bg-gradient-to-br from-30% via-80% dark:from-black dark:via-neutral-900 dark:to-teal-900 ">
+        <div className="container mx-auto w-11/12 md:w-8/12 lg:w-5/12 2xl:w-4/12">
+          <div className="mb-28 flex flex-col items-center">
+            <img
+              src={judiImage}
+              alt="Picture of Judi."
+              className="mb-10 mt-16 w-6/12 rounded-full md:w-4/12"
+            />
+            <h1 className="mb-2 font-serif text-5xl font-bold dark:text-rose-500">
+              Judith Böhlert
+            </h1>
+            <h2 className="text-3xl font-bold dark:text-rose-500 ">
+              Freelance Sofware Engineer
+            </h2>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <h3 className="mb-4 text-3xl font-bold dark:text-white">
+              Hi, I&apos;m Judith.
+            </h3>
+            <p className=" text dark:text-white">
+              Heres comes some text about me me. I shall describe my freelance
+              services. And tell a bit about myself. Mention how I can help
+              teams and projects. Some personal stuff as well. Here is more text
+              so we can test t the scrolling. And More. And more.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,17 +19,22 @@ export default {
         900: "#143569"
       },
       teal: {
-        100: "#CDE9F3",
-        500: "#3586A5",
-        900: "#054963"
+        100: "#90D0DA",
+        500: "#398D99",
+        900: "#02444D"
       },
       rose: {
-        100: "#FFF1FD",
-        500: "#DDA9D5",
-        900: "#B066A5"
+        100: "#FB9DEC",
+        500: "#E051C9",
+        900: "#B01097"
       }
     },
-    extend: {}
+    extend: {
+      fontFamily: {
+        serif: ['"Noto Serif"', "serif"],
+        sans: ['"Noto Sans"', "sans-serif"]
+      }
+    }
   },
   plugins: []
 };
